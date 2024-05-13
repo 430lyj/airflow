7cde02e9182bb487ecdc8bb3b1c79e99d2d97d94
430lyj <430lyj@gmail.com>
2024-05-13 19:18:31 +0900

    파일 디렉토리 변경

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -6,17 +6,17 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id = "dags_bash_select_fruit",
     schedule="10 0 * * 6#1",
-    start_date=pendulum.datetime(2024, 5, 13, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     t1_orange = BashOperator(
         task_id="t1_orange",
-        bash_command="/opt/airflow/plugins/shell/select_fuit.sh ORANGE" #워커 컨테이너가 실행 위치를 알 수 있도록
+        bash_command="/opt/airflow/plugins/.shell/select_fruit.sh ORANGE" #워커 컨테이너가 실행 위치를 알 수 있도록
     )
 
     t2_avocado = BashOperator(
         task_id="t2_avocado",
-        bash_command="/opt/airflow/plugins/shell/select_fuit.sh ORANGE"
+        bash_command="/opt/airflow/plugins/.shell/select_fruit.sh ORANGE"
     )
-    
+
     t1_orange >> t2_avocado
